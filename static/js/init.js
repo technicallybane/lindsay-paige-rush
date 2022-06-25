f557a6434adf4ce124f5867a80cf2c71cf5ca85e
Scott Bane <scottjbane@gmail.com>
2022-06-25 16:11:54 -0400

    Contact updates
    Updating the contact form to use AJAX and hopefully create a thank you message alert on submit

--- static/js/init.js
+++ static/js/init.js
@@ -507,45 +507,14 @@ function dizme_tm_data_images(){
 
 function dizme_tm_contact_form(){
 
-	"use strict";
+	$("contactForm").submit(function(e) {
+		e.preventDefault();
 
-	jQuery(".contact_form #send_message").on('click', function(){
-
-		var name 		= jQuery(".contact_form #name").val();
-		var email 		= jQuery(".contact_form #email").val();
-		var message 	= jQuery(".contact_form #message").val();
-		var subject 	= jQuery(".contact_form #subject").val();
-		var success     = jQuery(".contact_form .returnmessage").data('success');
-
-		jQuery(".contact_form .returnmessage").empty(); //To empty previous error/success message.
-		//checking for blank fields
-		if(name===''||email===''||message===''){
-
-			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
-		}
-		else{
-			// Returns successful data submission message when the entered information is stored in database.
-			jQuery.post("",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
-
-				jQuery(".contact_form .returnmessage").append(data);//Append returned message to message paragraph
-
-
-				if(jQuery(".contact_form .returnmessage span.contact_error").length){
-					jQuery(".contact_form .returnmessage").slideDown(500).delay(2000).slideUp(500);
-				}else{
-					jQuery(".contact_form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
-					jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
-				}
-
-				if(data===""){
-					jQuery("#contact_form")[0].reset();//To reset form fields on success
-				}
-
-			});
-		}
-		return false;
+		var $form = $(this);
+		$.post($form.attr("action"), $form.serialize()).then(function() {
+			alert("Thank You!");
+		});
 	});
-}
 
 // -----------------------------------------------------
 // ----------------    OWL CAROUSEL    -----------------
